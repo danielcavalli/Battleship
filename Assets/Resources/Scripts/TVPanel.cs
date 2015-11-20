583599dc4d8bbc881fb9d0a236c25465f4b5d341
Victor Fernandes <victorfernandes.nave@gmail.com>
2015-11-20 12:37:23 -0200

    HUD and fonts
    Added some HUD and old computer fonts.

---
+++ Assets/Resources/Scripts/TVPanel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+
+public class TVPanel : MonoBehaviour 
+{
+	public Shader shader;
+	private Material _material;
+	
+	protected Material material
+	{
+		get
+		{
+			if (_material == null)
+			{
+				_material = new Material(shader);
+				_material.hideFlags = HideFlags.HideAndDontSave;
+			}
+			return _material;
+		}
+	}
+	
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		if (shader == null) return;
+		Material mat = material;
+		Graphics.Blit(source, destination, mat);
+	}
+	
+	void OnDisable()
+	{
+		if (_material)
+		{
+			DestroyImmediate(_material);
+		}
+	}
+}
