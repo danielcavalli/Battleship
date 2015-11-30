eb58e63886d6684df060a87ecd4fa5922321fa88
Daniel Cavalli <daniel.cavalli@outlook.com>
2015-11-30 12:12:45 -0200

    Php fix

--- Assets/Server/default.php
+++ Assets/Server/default.php
@@ -1,17 +1,22 @@
-php
+<?php
 
 $servername = 'mysql.hostinger.com.br';
-$username = 'u186988407_admin';
-$password = 'battleship123';
-$dbname = 'u186988407_bship';
+$username = 'u913192727_jamv';
+$password = 'comituairmã2X';
+$dbname = 'u913192727_luzon';
 $canSubscribe = false;
-
-if($_POST[action] != null && $_POST[action] != )
+/* 
+Subscribe
+create session
+join session
+ingame_moves
+*/
+if($_POST['action'] != null && $_POST['action'] != )
 {
-   switch($_POST[action]){
-    case Subscribe
+   switch($_POST['action']){
+    case 'Subscribe':
     checkIfPlayerAlreadyExists();
-    SubscribeNewPlayer();
+    if($canSubscribe)SubscribeNewPlayer();
     break;
    }
 }
@@ -21,10 +26,10 @@ function checkIfPlayerAlreadyExists()
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn-connect_error) {
 			die(Connection failed  . $conn-connect_error);
-			 echo Didn't worked;
+			 echo 'Didn@\'t worked';
 	} 
 
-	$sql = SELECT Username FROM Players WHERE Username = .$_POST['username'].;;
+	$sql = "SELECT Username FROM Players WHERE Username = ".$_POST['username'].";";
 	$result = $conn-query($sql);
 
 		if ($result-num_rows == 0) {
@@ -38,14 +43,14 @@ function checkIfPlayerAlreadyExists()
 
 function SubscribeNewPlayer()
 {
-	echo Started subscribing;
+	echo "Started subscribing";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn-connect_error) {
 			die(Connection failed  . $conn-connect_error);
-	echo Didn't worked;
+	echo "bad";
 	} 
-	$sql = INSERT INTO u186988407_bship.Players(ID, Username, Hash,State,Status)
-	VALUES (NULL, '.$_POST['username'].','. $_POST['hash'].','.$_POST['state'].','.$_POST['status'].');;
+	$sql = "INSERT INTO ".$dbname.".Players(ID, Username, Hash,State,Status)
+	VALUES (NULL,".$_POST['username'].",".$_POST['hash'].",".$_POST['state'].",".$_POST['status'].");";
 
 	if ($conn-query($sql) === TRUE) {
 			echo New record created successfully;
@@ -54,6 +59,4 @@ function SubscribeNewPlayer()
 	}
 	$conn-close();
 }
-
-
-						
+?>
